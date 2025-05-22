78944723f9b6a07c06767f73b07b91d05d49e5b2
yasminlema <59012036+yasminlema@users.noreply.github.com>
2025-05-22 17:46:31 +0200

    Update FavoritoController.java

--- src/main/java/proyectoDam/PlanetaDigital/controller/FavoritoController.java
+++ src/main/java/proyectoDam/PlanetaDigital/controller/FavoritoController.java
@@ -24,12 +24,15 @@ public class FavoritoController {
     private LibroRepository libroRepository;
 
     @PostMapping("/corazonFavorito")
-    @Transactional
+    @Transactional // hace que todo lo que pasa en esta funcion se trate como una sola unidad de trabajo, por lo tanto si en algun momento del proceso falla algo este no guarda/cambia nada en la base de datos
+    // funcion que se ejecuta cada vez que pulsamos en el corazon de la pagina
     public String toggleFavorito(@RequestParam Integer usuarioCod,
                                  @RequestParam Integer libroCod) {
 
+        // comprueba si el usuario ya tiene el libro como favorito
         boolean existe = favoritoRepository.existsByUsuarioCodAndLibroCod(usuarioCod, libroCod);
 
+        // si ya lo tiene como favirito lo elimina, sino lo guarda como favorito
         if (existe) {
             favoritoRepository.deleteByUsuarioCodAndLibroCod(usuarioCod, libroCod);
         } else {
@@ -42,15 +45,18 @@ public class FavoritoController {
         return "redirect:/detalleLibro/" + libroCod;
     }
 
+    // funcion para mostrar todos los libros favoritos en la pagina
     @GetMapping("/misFavoritos")
     public String verFavoritos(HttpSession session, Model model) {
-        Integer usuarioCod = (Integer) session.getAttribute("usuarioCod");
+        Integer usuarioCod = (Integer) session.getAttribute("usuarioCod"); // recoge el usuariocod de la sesion
+        // si no hay usuariocod, es decir la sesion no esta iniciada redirige el login
         if (usuarioCod == null) {
             return "redirect:/login";
         }
 
+        // obtiene la lista de libros favoritos
         List<Libro> librosFavoritos = favoritoRepository.findLibrosFavoritosByUsuarioCod(usuarioCod);
-        model.addAttribute("favoritos", librosFavoritos);
+        model.addAttribute("favoritos", librosFavoritos); // los añade al model para mostrarlos
         return "misFavoritos";
     }
 }

2176e29033a7da5f3e003fed5dac209c0b366bea
yasminlema <59012036+yasminlema@users.noreply.github.com>
2025-05-23 09:51:43 +0200

    Update ValoracionController.java

--- src/main/java/proyectoDam/PlanetaDigital/controller/ValoracionController.java
+++ src/main/java/proyectoDam/PlanetaDigital/controller/ValoracionController.java
@@ -25,19 +25,26 @@ public class ValoracionController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    // metodo que controla la valoracion de los libros
     @PostMapping("/valorarLibro")
     public String valorarLibro(@RequestParam int libroCod, HttpSession session, @RequestParam int valoracion) {
+        // recoge el usuariocod de la sesion
         Integer usuarioCod = (Integer) session.getAttribute("usuarioCod");
+        // si no encuentra el usuariocod, redirige al login
         if (usuarioCod == null) {
             return "redirect:/login";
         }
 
+        // busca el libro y usuario en la base de datos
         Libro libro = libroRepository.findById(libroCod).orElse(null);
         Usuario usuario = usuarioRepository.findById(usuarioCod).orElse(null);
+        // si alguno no existe o la valoracion se sale del rango redirige con error
         if (libro == null || usuario == null || valoracion < 1 || valoracion > 5) {
             return "redirect:/detalleLibro/" + libroCod + "?error";
         }
 
+        // si la valoracion existe se actualiza la valoracion
+        // si no existe se crea una nueva y se gusrda en la BD
         Optional<Valoracion> existente = valoracionRepo.findByLibro_LibroCodAndUsuario_UsuarioCod(libroCod, usuarioCod);
         Valoracion valoracionObj = existente.orElse(new Valoracion());
         valoracionObj.setLibro(libro);

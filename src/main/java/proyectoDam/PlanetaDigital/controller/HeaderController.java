0efe18408bb12540c4290b95d2ac5b811d608bcf
yasminlema <59012036+yasminlema@users.noreply.github.com>
2025-05-23 08:32:29 +0200

    Update HeaderController.java

--- src/main/java/proyectoDam/PlanetaDigital/controller/HeaderController.java
+++ src/main/java/proyectoDam/PlanetaDigital/controller/HeaderController.java
@@ -14,15 +14,17 @@ public class HeaderController {
 
     @ModelAttribute("usuarioNombre")
     public String populateUsuarioNombre(HttpSession session) {
-        Integer usuarioCod = (Integer) session.getAttribute("usuarioCod");
+        Integer usuarioCod = (Integer) session.getAttribute("usuarioCod"); // saca el usuariocod de la sesion
+        // si en la sesion encuentra un usuariocod
         if (usuarioCod != null) {
-            return usuarioRepository.findById(usuarioCod)
-                    .map(u -> u.getUsuNombre())
-                    .orElse("Usuario conectado");
+            return usuarioRepository.findById(usuarioCod) // busca ese usuariocod en la BD
+                    .map(u -> u.getUsuNombre()) // si lo encuentra devuelve su nombre
+                    .orElse("Usuario conectado"); // si no lo encuentra devuelve usuario conectado
         }
         return "Usuario conectado";
     }
 
+    // este metodo recupera el tipocuenta de la sesion y despues lo usamos en la vista para mostrar u ocultar cosas segun el tipo de cuenta del usuario
     @ModelAttribute("tipocuenta")
     public Integer populateTipoCuenta(HttpSession session) {
         return (Integer) session.getAttribute("tipocuenta");
